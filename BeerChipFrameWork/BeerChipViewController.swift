2213724121c2d3561992bdfa2471930fa0fa2916
Srinivasulu3264 <srinivasulu.budharapu@vmokshagroup.com>
2018-02-08 10:31:31 +0530

    added code for dynamic placement of framework in BeerchipViewcontroller

--- BeerChipFrameWork/BeerChipViewController.swift
+++ BeerChipFrameWork/BeerChipViewController.swift
@@ -19,28 +19,37 @@ public class BeerChipViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    public func openBeerChipFrameWork(viewController: UIViewController){
+     public func openBeerChipFrameWork(viewController: UIViewController , withLocation:CGRect){
         
         beerchipFrameWorkVC = viewController
         
-        let mainView = UIView(frame:CGRect(x:0 , y:250, width: UIScreen.main.bounds.width , height: 70))
-        mainView.backgroundColor = .lightGray
+        let  location:CGRect = withLocation
+        
+        let mainView = UIView(frame:CGRect(x: location.origin.x , y: location.origin.y, width: UIScreen.main.bounds.width-20 , height: location.size.height))
+        mainView.backgroundColor = UIColor(red: 0.0 ,green: 94.0/255.0, blue: 163.0/255.0, alpha:1.0 )
         viewController.view.addSubview(mainView)
-
-        let webViewButton = UIButton(frame: CGRect(x: 20 ,y: 15, width: 150, height: 40))
+        
+        //   let webViewButton = UIButton(frame: CGRect(x: 15 ,y: 7, width: 150, height: 26))
+        let webViewButton = UIButton()
+        let buttonHeight = location.size.height * 0.75
+        webViewButton.frame = CGRect(x:15 ,y: ((mainView.center.y-location.origin.y)-(buttonHeight/2)), width: 150, height: buttonHeight )
         webViewButton.setTitle("Open WebView", for: .normal)
-        webViewButton.backgroundColor = UIColor(red: 0.0 ,green: 94.0/255.0, blue: 163.0/255.0, alpha:1.0 )
-        webViewButton.setTitleColor(.white, for: .normal)
+        webViewButton.backgroundColor = .white
+        webViewButton.setTitleColor(UIColor(red: 0.0 ,green: 94.0/255.0, blue: 163.0/255.0, alpha:1.0 ), for: .normal)
+        webViewButton.layer.cornerRadius = 8.0
         webViewButton.addTarget(self, action: #selector(gotoWebView), for: .touchUpInside)
         mainView.addSubview(webViewButton)
         
-        let alertButton = UIButton(frame: CGRect(x: 200 ,y: 15, width: 150, height: 40))
+        //      let alertButton = UIButton(frame: CGRect(x: 185 ,y: 7, width: 150, height: 26))
+        let alertButton = UIButton( )
+        alertButton.frame = CGRect(x:185 ,y: ((mainView.center.y-location.origin.y)-(buttonHeight/2)), width: 150, height: buttonHeight )
         alertButton.setTitle("Click Here", for: .normal)
-        alertButton.backgroundColor = UIColor(red: 0.0 ,green: 94.0/255.0, blue: 163.0/255.0, alpha:1.0 )
-        alertButton.setTitleColor(.white, for: .normal)
+        alertButton.backgroundColor = .white
+        alertButton.setTitleColor(UIColor(red: 0.0 ,green: 94.0/255.0, blue: 163.0/255.0, alpha:1.0 ), for: .normal)
+        alertButton.layer.cornerRadius = 8.0
         alertButton.addTarget(self, action: #selector(showTheAlertMessage), for: .touchUpInside)
         mainView.addSubview(alertButton)
-    
+
     }
 
     @objc func gotoWebView ()  {
